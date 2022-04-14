3b22352b65e2045c142ae882e25d329130ee2539
MaxBing63 <maxbing63@gmail.com>
2022-04-14 18:43:31 +0300

    Update CSS styling and create a loop to fill main div with squares

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let squaresContainer = document.querySelector('.squares');
 
 for(let i = 0; i < 16*16; i++) {
-       
+    let square = document.createElement('div');
+    square.classList.add('square');
+    squaresContainer.appendChild(square);
 }
