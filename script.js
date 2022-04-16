fa784476b886c9e2f9754dbcdcb3c2a58a719c61
MaxBing63 <maxbing63@gmail.com>
2022-04-16 17:06:25 +0300

    Make clear button reset the grid's square colors using a loop

--- script.js
+++ script.js
@@ -7,8 +7,18 @@ for(let i = 0; i < 16*16; i++) {
 }
 
 const squares = document.querySelectorAll('.square');
-
 squares.forEach(el => 
     el.addEventListener('mouseover', () => {
         el.style.backgroundColor = 'red';
-    }))
+    }));
+    
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', clearGrid);
+
+function clearGrid(){
+    const colored = document.querySelectorAll('.square');
+    for(let i = 0; i < colored.length; i++){
+        let square = colored[i];
+        square.style.backgroundColor = '';
+    }
+}
